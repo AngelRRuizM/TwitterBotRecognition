a3b655f641efdd33f2d7c0c243e3001dbbccd77e
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2019-01-24 16:43:46 -0600

    Added twitter test and train datasets. Meaningcloud hello world

---
+++ analysis.py
@@ -0,0 +1,16 @@
+import sys
+import meaningcloud
+import requests
+import json
+
+url = "https://api.meaningcloud.com/sentiment-2.1"
+
+x = {'key': '8601f345cb8241110b493ab5e4fc7736', 
+    'lang': 'auto',
+    'txt': '@TayaPYT thanx babe .. Xoxo .. See u in a second',
+    'ilang': 'en'}
+headers = {'content-type': 'application/json'}
+
+response = requests.request("POST", url, data=json.dumps(x), headers=headers)
+
+print(response.text)
